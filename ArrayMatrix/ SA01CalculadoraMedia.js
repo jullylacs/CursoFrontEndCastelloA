7e9940fd0be01202772872f8fc5bc43aa9e246ed
jullylacs <julialacerdasil@gmail.com>
2025-03-12 11:32:19 -0300

    12.03.25

---
+++ ArrayMatrix/ SA01CalculadoraMedia.js
@@ -0,0 +1,41 @@
+//situação de aprendizagem 01 - Array e Matrix
+//Calculadora de Média
+const prompt = requir("prompt-sync")();
+
+var notas = []; //declaração de um vetor
+
+//função para adiocionar nota -> media
+function adicionarNota(){
+    let nota = Number(prompt("Informe a nota: "))
+    media.push(nota);
+    console.log("Nota adicionada com sucesso");
+}
+//função para calcular a média
+function calcularMedia(){
+    if(notas.length === 0) return;
+    let soma = notas.reduce((x,y)=>x+y);
+    let mediaFinal = (soma/notas.length);
+    console.log("A média é"+mediaFinal)
+}
+
+//menu de opções
+function menuOpcoes(){
+    let continuar = true;
+    while(continuar){
+    console.log("===Calculadora Média===");
+    console.log("|1. Adicionat Nota     |");
+    console.log("|2. Calcular Média     |");
+    console.log("|3. Sair               |");
+    console.log("========================");
+    let operacao = prompt("Informe a Opção Desejada")
+    switch(operacao){
+        case "1": adicionarNota();break;
+        case "2": calcularMedia();break;
+        case "3": continuar= false;break;
+        default: console.log("Opção inválida");break;
+    }//fim do switch
+  }//fim do while
+  console.log("Saindo...");
+}
+
+menuOpcoes();
--- ArrayMatrix/script01.js
+++ ArrayMatrix/script01.js
@@ -1,7 +1,19 @@
-//Métodos avançados de Arrays
+//Metodos Avançados de Arrays
+let numeros = [10, 20, 30, 40, 50];
 
-//map
+//Map - percorre o array e realiza operações
+let numerosDobro = numeros.map(x => x*2);
+console.log(numerosDobro);
 
-//filter
+//Filter - percorrer e filtrar
+let numerosFiltro = numeros.filter(x => x>25);
+console.log(numerosFiltro);
 
-//reduce
+//combinar filter com map
+let numFiltroDobro = numeros.filter(x => x<35).map(x => x/10);
+console.log(numFiltroDobro)
+
+//Reduce -> array -> simples
+//soma dos valores de um array
+let soma = numeros.reduce((x,y) => (x+y));
+console.log(soma);
