9754c57d7f3b68d7fdab0470d28f6d5ed6d23bc6
jullylacs <julialacerdasil@gmail.com>
2025-06-04 11:31:14 -0300

    04.06.25

--- FrameWorks/Angular16/app_rh/src/app/app.module.ts
+++ FrameWorks/Angular16/app_rh/src/app/app.module.ts
@@ -3,14 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './templates/header/header.component';
+import { FooterComponent } from './templates/footer/footer.component';
+import { VagasComponent } from './views/vagas/vagas.component';
+import { PainelVagasComponent } from './views/painel-vagas/painel-vagas.component';
+import { CurriculosComponent } from './views/curriculos/curriculos.component';
+import { HomeComponent } from './views/home/home.component';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    VagasComponent,
+    PainelVagasComponent,
+    CurriculosComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule //módulo necessário para que a página faça as requisições HttpClient
   ],
   providers: [],
   bootstrap: [AppComponent]
